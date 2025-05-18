2b608a5b94a7b75c2b49fd772e5c2681dc277526
Ben-MR <b.rieck@web.de>
2025-05-18 11:59:44 +0200

    deleted help functions

--- models/drawable.-object.class.js
+++ models/drawable.-object.class.js
@@ -29,38 +29,6 @@ class DrawableObject {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);     
     }
 
-    drawFrame(ctx) { //Kästen für Kollisionsabfrage
-        if(this instanceof Character || this instanceof Chicken || this instanceof Endboss || this instanceof ThrowableObjects || this instanceof CollectableObjectsCoins || this instanceof CollectableObjectsBottles) {
-            ctx.beginPath();
-            ctx.lineWidth = '3';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();  
-        }
-    }
-
-    drawFrame2(ctx) {
-        if (
-            this instanceof Character ||
-            this instanceof Chicken ||
-            this instanceof Endboss ||
-            this instanceof ThrowableObjects ||
-            this instanceof CollectableObjectsCoins ||
-            this instanceof CollectableObjectsBottles ||
-            this instanceof SmallChicken
-        ) {
-            ctx.beginPath();
-            ctx.lineWidth = '3';
-            ctx.strokeStyle = 'red';    
-            const x = this.x + (this.offset?.left || 0);
-            const y = this.y + (this.offset?.top || 0);
-            const width = this.width - ((this.offset?.left || 0) + (this.offset?.right || 0));
-            const height = this.height - ((this.offset?.top || 0) + (this.offset?.bottom || 0));    
-            ctx.rect(x, y, width, height);
-            ctx.stroke();
-        }
-    }
-
     /**
      * Loads multiple images into the image cache.
      * Used for animation frames or image states.
--- models/world.class.js
+++ models/world.class.js
@@ -229,8 +229,6 @@ class World {
             this.flipImage(mo);
         }
         mo.draw(this.ctx);
-        // mo.drawFrame(this.ctx);
-        //mo.drawFrame2(this.ctx);
         if(mo.otherDirection) {
             this.flipImageBack(mo);
         }
